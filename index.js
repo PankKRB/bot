f8480ea1a2fafaf079167c120f40cafe7ace1632
PankKRB <87706843+PankKRB@users.noreply.github.com>
2023-11-07 12:45:04 +0700

    Add files via upload

---
+++ index.js
@@ -0,0 +1,17 @@
+require('dotenv/config');
+const { Client, IntentsBitField } = require('discord.js');
+const { CommandHandler } = require('djs-commander');
+const path = require('path');
+const { MessageEmbed } = require("discord.js"); // Đừng quên import MessageEmbed
+
+const client = new Client({
+  intents: [IntentsBitField.Flags.Guilds],
+});
+
+new CommandHandler({
+  client,
+  commandsPath: path.join(__dirname, 'slash-commands'),
+  eventsPath: path.join(__dirname, 'events'),
+});
+
+client.login(process.env.TOKEN);
